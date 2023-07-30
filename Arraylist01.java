527063b436eb007738ab81792ac314550b30e451
rukhsarparvin <124374355+rukhsarparvin@users.noreply.github.com>
2023-07-30 23:49:54 +0530

    Add files via upload

---
+++ Arraylist01.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class ArrayList{
+	public static void main(String[] args){
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("ruku");
+		System.out.println(list);
+	}
+}
