43c210ed261edbb97451d2b37f072d334a700374
rukhsarparvin <124374355+rukhsarparvin@users.noreply.github.com>
2023-07-30 23:53:38 +0530

    Add files via upload

---
+++ Arraylist04.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class Array{
+	public static void main(String[] args){
+ 		ArrayList<Object> al = new ArrayList<>();
+	
+// counting  object
+		// System.out.println(al.size);
+		// System.out.println(al.Size());
+
+// printing
+		System.out.println(al);
+		System.out.println("b");
+
+// additing object
+		al.add("a");
+		System.out.println("a");
+
+// searching object
+		System.out.println(al.contains("a"));
+		// System.out.println(a1.contains(new String("a")));
+
+// removing
+ 		al.remove("a");
+		System.out.println(a;);
+
+	}
+}
+ 		
+		
+ 
